3a5fb640bde78f79a15797ac6dd42ba27b593c1b
Michael Klein <Michaelklein1997@gmail.com>
2022-04-26 09:59:43 -0500

    completed module_1

--- 01-accelerated-java/module_1/09-stu-method-practice/starter/MethodPractice/src/main/java/com/company/App.java
+++ 01-accelerated-java/module_1/09-stu-method-practice/starter/MethodPractice/src/main/java/com/company/App.java
@@ -1,27 +1,56 @@
 package com.company;
 
+import java.util.List;
+import java.util.List;
+
 public class App {
 
 
 	// subtract
-
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
 
 
 	// subtractOrZero
-
-
+    public static int subtractOrZero(int a, int b) {
+        if (b > a) {
+            return 0;
+        } else {
+            return a-b;
+        }
+    }
 
 	// max
-
+    public static int max(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > a && b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 
 
 	// min
-
+    public static int min(int a, int b, int c) {
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 
 
 	// isLarger
-
-
-
-
+    public static boolean isLarger(int a, int b) {
+        if (a > b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
